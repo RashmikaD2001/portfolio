850461db20f730c18463c9b3f45c815a83a79ead
RashmikaD2001 <rashmika.d2167@gmail.com>
2025-06-22 19:24:36 +0530

    loading screen

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import LoadingScreen from './components/LoadingScreen';
+import './index.css'
+
+function App() {
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <>
+      {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}{" "}
+        <div className={`min-h-screen transition-opacity duration-700 ${isLoading ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
+
+        </div>
+    </>
+  )
+}
+
+export default App
