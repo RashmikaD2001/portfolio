cc0a4e01405bbdb266f2b90f1a734d52d5aac6ac
RashmikaD2001 <rashmika.d2167@gmail.com>
2025-08-14 10:03:01 +0530

    add new project

--- src/components/section/FeaturedProjects.jsx
+++ src/components/section/FeaturedProjects.jsx
@@ -14,13 +14,13 @@ const FeaturedProjects = () => {
                         <Project
                             title='Bank System Backend'
                             description='Java Spring Boot backend to manage bank users, accounts and loans. Machine learning solution to approve the loans automatically using Python and Scikit-learn.'
-                            skills={["Java Spring Boot", "Python", "Scikit-learn", "MSSQL", "Microsoft Azure"]}
+                            skills={["Java Spring Boot", "Python", "Scikit-learn", "MSSQL", "Microsoft Azure", 'Git & Github']}
                             path='https://github.com/RashmikaD2001/Bank-System-Backend'
                         />
                         <Project
                             title='Focus App'
                             description='FOCUS App: A Social Media Distraction Reduction System using Kotlin, Arduino & Human-Computer Interaction Techniques'
-                            skills={['Kotlin', 'Arduino', 'C language', 'Android']}
+                            skills={['Kotlin', 'Arduino', 'C language', 'Android', 'Git & Github']}
                             path='https://github.com/RashmikaD2001/Human-Computer-Interaction-Project'
                         />
                         <Project
@@ -28,6 +28,12 @@ const FeaturedProjects = () => {
                             description='Final Project of Python Bootcamp organized by PyData Community. The project focuses on data analysis based on cricket world cup data from 1975 to 2023 and sentiment analysis using techniques from natural language processing (NLP)'
                             skills={['Python', 'Jupyter Notebook', 'HuggingFace', 'Git & Github']}
                             path='https://github.com/RashmikaD2001/Python-Bootcamp-Project'
+                        />
+                        <Project
+                            title="HarmonyBot: A Conflict Mediating Social Robot"
+                            description='a robot that detects the emotional tone of a conversation and uses gentle prompts, tone shifts, and humor or empathy to de-escalate tension and promote calm communication between people'
+                            skills={['Python','Arduino','C language','Azure Cloud Service', 'HuggingFace', 'Git & Github']}
+                            path='https://github.com/RashmikaD2001/CSC-465-2.0-Robotics'
                         />
                     </div>
                 </div>
